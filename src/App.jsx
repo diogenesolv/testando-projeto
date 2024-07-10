3ca04bdc61200233b5cdef4ec388f015739b6fff
Diógenes Sousa <diogenesdinhoo@gmail.com>
2024-07-10 15:39:18 -0300

    alterando no remoto

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>oiiii</h2>
-      <h1>Vite + React</h1>
+      <h1>remoto para o local</h1>
 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
